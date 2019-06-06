947363ec5b9070f9434e844069150972f82e7a21
danielbrog <brog.daniel@gmail.com>
2019-06-06 16:14:56 -0400

    Boilerplate React app

---
+++ README.md
@@ -0,0 +1,2 @@
+# ReactApps
+Learning react, planning on eventually merging with the node site to have a full stack site.
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+//renders root class
+ReactDOM.render(<p>Boiler plate</p>, document.getElementById('app'))
