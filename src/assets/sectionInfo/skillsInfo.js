fc0142e065f25e4f4579767eef52b500129dc26f
danielbrog <brog.daniel@gmail.com>
2019-09-09 17:57:24 -0400

    added baseball app link

--- src/assets/sectionInfo/skillsInfo.js
+++ src/assets/sectionInfo/skillsInfo.js
@@ -1,45 +1,28 @@
-const skillsInfo= [{
-    size: "large",
+const skillsInfo = [
+  {
+    size: "small",
     title: "Front-End",
     image: "./images/frontEndIcon.webp",
-    list: [
-        'HTML',
-        'CSS',
-        'SASS/SCSS',
-        'Javascript',
-        'React',
-    ]
-},{
-    size: "large",
+    list: ["HTML", "CSS", "SASS/SCSS", "Javascript", "React"]
+  },
+  {
+    size: "small",
     title: "Back-End",
     image: "./images/backEndIcon.webp",
-    list: [
-        'Node.JS',
-        'Express',
-        'Java',
-        'Ruby on Rails'
-    ]
-},{
-    size: "large",
+    list: ["Node.JS", "Express", "Java", "Ruby on Rails"]
+  },
+  {
+    size: "small",
     title: "Database",
     image: "./images/databaseIcon.webp",
-    list: [
-        'MySQL',
-        'MongoDB',
-        'DB2',
-        'Microsoft SQL Server'
-    ]
-},{
-    size: "large",
+    list: ["MySQL", "MongoDB", "DB2", "Microsoft SQL Server"]
+  },
+  {
+    size: "small",
     title: "Supporting",
     image: "./images/supportIcon.webp",
-    list: [
-        'Git',
-        'ES6',
-        'Jest/Testing',
-        'NGinx',
-        'Apache'
-    ]
-}]
+    list: ["Git", "ES6", "Jest/Testing", "NGinx", "Apache"]
+  }
+];
 
-export default skillsInfo
+export default skillsInfo;
